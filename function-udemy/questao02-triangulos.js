954c2fae03c3a3680ff416fde1123520fb94ede5
Arthur Cavalcante <linsarthur02@gmail.com>
2024-09-27 18:58:00 -0300

    Questões 01 e 02 udemy function

---
+++ function-udemy/questao02-triangulos.js
@@ -0,0 +1,14 @@
+
+function verificarTriangulo(a, b, c){
+    if(a === b && b === c){
+        console.log("Triangulo Equilátero");
+    }else if(a === b && b !== c){
+        console.log("Triangulo Isósceles");
+    }else if(a !== b && b !== c){
+        console.log("Triangulo Escaleno");
+    }
+
+    return verificarTriangulo
+}
+
+verificarTriangulo(10, 12, 11)
