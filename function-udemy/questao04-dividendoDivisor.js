679b1ec4fc01fc7cf159ad339d5957a3e998481f
Arthur Cavalcante <linsarthur02@gmail.com>
2024-09-27 19:22:48 -0300

    Questao 04 udemy function

---
+++ function-udemy/questao04-dividendoDivisor.js
@@ -0,0 +1,10 @@
+function dividendoDivisor(dividendo, divisor){
+    
+    let resto = dividendo % divisor
+    let divisao = Math.floor(dividendo / divisor);
+    
+    console.log(resto);
+    console.log(divisao);
+    
+}   
+dividendoDivisor(20,4)
