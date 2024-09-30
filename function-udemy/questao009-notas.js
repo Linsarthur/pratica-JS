8677ee6c2d755e811f590527fb5509dbf75c6f35
Arthur Cavalcante <linsarthur02@gmail.com>
2024-09-30 17:10:14 -0300

    Questão da analise de notas - questao 009

---
+++ function-udemy/questao009-notas.js
@@ -0,0 +1,44 @@
+/*Construa uma função para um sistema de notas de uma instituição que possui a seguinte política de
+classificação: Todo aluno recebe uma nota de 0 a 100. Alunos com nota abaixo de 40 são reprovados. As notas
+possuem a seguinte regra de arredondamento: Se a diferença entre a nota e o próximo múltiplo de 5 for menor
+que 3, arredondar a nota para esse próximo múltiplo de 5. Se a nota for abaixo de 38, não é feito nenhum
+arredondamento pois esta nota resulta na reprovação do aluno. Por exemplo, a nota 84 será arredondada para
+85, mas a nota 29 não será arredondada por ser abaixo de 40 e não ser possível arredondamento eficiente, ou
+seja, que evite a reprovação do aluno. No caso de a nota ser 38, o arredondamento é possível pois atingirá 40
+e o aluno será aprovado.
+ */
+
+const students = [
+    {name: 'Arthur',grade: 60},
+    {name: 'Matheus',grade: 38},
+    {name: 'Davi',grade: 27},
+    {name: 'Julia',grade: 37}
+]
+
+
+function verifyGrade(notas) {
+
+    if (notas < 38) {
+        return notas
+    }
+    const nextMultiplies = Math.ceil(notas / 5) * 5;
+    if (nextMultiplies - notas < 3) {
+        return nextMultiplies
+    }
+    return notas
+
+
+}
+function avaliarAlunos(students) {
+    students.forEach(student => {
+        const notaAjustada = verifyGrade(student.grade)
+        const status = notaAjustada >= 40 ? 'Aprovado' : 'Reprovado';
+        console.log(`${student.name} - nota ${student.grade} - status ${status}`);
+
+    });
+}
+
+
+
+avaliarAlunos(students)
+
